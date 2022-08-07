37b65ee50f0c59bd7f7d57f9f9bf1a1873b50a43
sun <ub3132003@163.comm>
2022-08-07 16:50:39 +0800

    开始

---
+++ PhysicsSamples/Assets/Demos/Bee/Script/DroneComponent.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct DroneComponent : IComponentData
+{
+    public float3 Position;
+}
---
+++ PhysicsSamples/Assets/Demos/Bee/Script/PeriodicallySpawnDroneAuthoring.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using Unity.Assertions;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics.Systems;
+using Unity.Transforms;
+using UnityEngine;
+public struct PeriodicallySpawnDroneComponent : IComponentData, ISpawnSettings, IPeriodicSpawnSettings
+{
+    public Entity Prefab { get; set; }
+    public float3 Position { get; set; }
+    public quaternion Rotation { get; set; }
+    public float3 Range { get; set; }
+    public int Count { get; set; }
+
+    public int SpawnRate { get; set; }
+    public int DeathRate { get; set; }
+    public int Id;
+}
+/// <summary>
+/// 配置生成属性
+/// </summary>
+public class PeriodicallySpawnDroneAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public float3 Range;
+    public int SpawnRate;
+    public GameObject Prefab;
+    public int Count = 1;
+    public int Lifetime = 500;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new PeriodicallySpawnDroneComponent
+        {
+            Count = Count,
+            DeathRate = Lifetime,
+            Position = transform.position,
+            Prefab = conversionSystem.GetPrimaryEntity(Prefab),
+            Range = Range,
+            Rotation = quaternion.identity,
+            SpawnRate = SpawnRate,
+            Id = 0,
+        });
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs) => referencedPrefabs.Add(Prefab);
+}
+
+class PeriodicallySpawnDroneSystem : PeriodicalySpawnRandomObjectsSystem<PeriodicallySpawnDroneComponent>
+{
+    internal override void ConfigureInstance(Entity instance, ref PeriodicallySpawnDroneComponent spawnSettings)
+    {
+        var pos = EntityManager.GetComponentData<Translation>(instance);
+        EntityManager.AddComponentData<DroneComponent>(instance , new DroneComponent
+        {
+            Position = pos.Value
+        });
+    }
+}
